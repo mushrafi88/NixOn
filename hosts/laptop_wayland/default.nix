998d5f6fd1ca4ef6e119581491574be4bb3bed03
mushrafi88 <mushrafi88@gmail.com>
2023-07-12 12:43:43 +0600

    ibus

--- hosts/laptop_wayland/default.nix
+++ hosts/laptop_wayland/default.nix
@@ -54,7 +54,10 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    inputMethod.enabled = "ibus";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = [ ];
+    };
   };
 
   environment = {
