81db7456f029c3aa3b679132b9d27c6cf6b7d6ea
mushrafi88 <mushrafi88@gmail.com>
2024-04-06 00:19:19 +0600

    April Bonanza

--- hosts/laptop_wayland/default.nix
+++ hosts/laptop_wayland/default.nix
@@ -51,7 +51,6 @@
 
 
   services.xserver = {
-    exportConfiguration = true; # link /usr/share/X11/ properly
     xkb.layout = "us";
     xkb.options = "caps:escape";
   };
@@ -61,19 +60,18 @@
     keyMap = "us";                          # or us/azerty/etc
   };
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   services = {
