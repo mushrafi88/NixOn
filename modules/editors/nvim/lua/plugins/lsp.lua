7d0715a5d42a1ed56bf40dd1ecc547f723a18644
mushrafi88 <mushrafi88@gmail.com>
2024-04-09 05:25:02 +0600

    Latex Env

--- modules/editors/nvim/lua/plugins/lsp.lua
+++ modules/editors/nvim/lua/plugins/lsp.lua
@@ -37,7 +37,7 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
 require("luasnip.loaders.from_vscode").lazy_load() -- For friendly snippets, boilerplate JS, HTML, etc
 require'lspconfig'.jedi_language_server.setup{}
 require("luasnip.loaders.from_lua").lazy_load()
--- require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
+require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip_snippets/custom_lua/"})
 
 -- for consistency
 local diagnostic = vim.diagnostic
