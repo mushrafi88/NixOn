9ea627aa5031c1d2f0fc84a64cfe1a27c8003ce7
mushrafi88 <mushrafi88@gmail.com>
2024-04-10 01:41:56 +0600

    animdl Env

--- modules/scripts/animdl.nix
+++ modules/scripts/animdl.nix
@@ -16,18 +16,33 @@ let
   #  anime_downloader_daily = pkgs.writeShellScriptBin "anime_downloader_daily" '' 
 
   anime_list_update = pkgs.writeShellScriptBin "anime_list_update" '' 
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
-inotifywait -m -e delete --format '%w%f' "$ANIME_DOWNLOAD_FOLDER" -r | while read -r FILE
-do
+ANIME_FOLDER="/mnt/media_m/Anime"
+
+# Function to update Anilist
+update_anilist() {
+    local FILE="$1"
+    local ANIME_NAME
+    local EPISODE_NUMBER
+    
     # Check if the deleted file is a video
     if [[ "$FILE" =~ \.(mp4|mkv|ts|avi)$ ]]; then
         # Parse the anime name and episode number from the deleted file's path
         ANIME_NAME=$(basename "$(dirname "$FILE")")
         EPISODE_NUMBER=$(echo "$FILE" | grep -oP '(E|Episode\s|episode)[0-9]+' | grep -oP '[0-9]+')
-        $HOME/.config/animdl/anilist_update.py "$ANIME_NAME" "$EPISODE_NUMBER"  # Replace with your Python script path # Replace with your Python script path
+        $HOME/.config/animdl/anilist_update.py "$ANIME_NAME" "$EPISODE_NUMBER"  # Replace with your Python script path
     fi
-done
+}
+
+# Monitor ANIME_DOWNLOAD_FOLDER
+inotifywait -m -e delete --format '%w%f' "$ANIME_FOLDER" -r | while read -r FILE
+do
+    update_anilist "$FILE"
+done 
+
+# Wait for background processes to finish
+wait
 
 '';
 in
